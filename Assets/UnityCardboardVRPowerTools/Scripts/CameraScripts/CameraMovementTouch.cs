66cff59990dbb96b9f606216679b78bc8b4a90dc
Neengash <pepmanas93@gmail.com>
2021-06-02 22:00:54 +0200

    Finished FreeMovementCamera Scene Demo

---
+++ Assets/UnityCardboardVRPowerTools/Scripts/CameraScripts/CameraMovementTouch.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementTouch : MonoBehaviour
+{
+    [SerializeField]
+    Transform cameraBase;
+
+    [SerializeField]
+    Transform cameraTransform;
+
+    [SerializeField, Range(0.5f, 10f)]
+    float cameraSpeed;
+
+    void Update()
+    {
+        if (
+            Google.XR.Cardboard.Api.IsTriggerPressed || 
+            Input.GetButton("Fire1")
+        ) {
+            DoMovement();
+        }
+    }
+
+    void DoMovement() {
+        Vector3 direction = new Vector3(
+            cameraTransform.forward.x,
+            0,
+            cameraTransform.forward.z
+        );
+        cameraBase.Translate(direction.normalized * cameraSpeed * Time.deltaTime);
+    }
+}
