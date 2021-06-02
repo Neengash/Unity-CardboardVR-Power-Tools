74bdde442bad0c6885549e022325758e11653b11
Neengash <pepmanas93@gmail.com>
2021-06-02 12:37:08 +0200

    Added structure to disable checks for vrInteratcions

--- Assets/UnityCardboardVRPowerTools/Scripts/CameraScripts/VRPointer.cs
+++ Assets/UnityCardboardVRPowerTools/Scripts/CameraScripts/VRPointer.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 public class VRPointer : MonoBehaviour
 {
+    public bool ActiveVRInteractions = true;
+
     [SerializeField]
     private float pointerDistance = 5;
     private VRInteraction gazedObject = null;
     private RaycastHit hit;
 
     void Update() {
+        if (ActiveVRInteractions) {
+            RaycastInteractions();
+        }
+    }
+
+    void RaycastInteractions() {
         if (Physics.Raycast(transform.position, transform.forward, out hit, pointerDistance)) {
             // Can't use null conditional on gazedObject in the if because it could raise MissingReferenceException
             if (hit.transform.gameObject != (gazedObject == null ? null : gazedObject.gameObject)) {
