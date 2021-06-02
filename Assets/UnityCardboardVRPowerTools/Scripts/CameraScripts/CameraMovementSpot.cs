df0df73c068901e9952c039bf46a42df1052cafc
Neengash <pepmanas93@gmail.com>
2021-06-03 01:01:41 +0200

    spot movement now works ... dont know why

--- Assets/UnityCardboardVRPowerTools/Scripts/CameraScripts/CameraMovementSpot.cs
+++ Assets/UnityCardboardVRPowerTools/Scripts/CameraScripts/CameraMovementSpot.cs
@@ -4,30 +4,38 @@ public class CameraMovementSpot : MonoBehaviour
 {
     public static CameraMovementSpot instance;
     [SerializeField, Range(1f, 10f)] float speed = 1f;
+
+    [SerializeField]
+    Transform baseTransform; 
     private Vector3 destination;
     private bool moving;
 
     void Start() {
         instance = this;
         moving = false;
+        Debug.Log("Start - speedValue = " + speed);
     }
 
     void Update() {
+        Debug.Log("Update - Start - movingValue = " + moving.ToString());
         if (moving) {
-            transform.position = Vector3.MoveTowards(transform.position, destination, speed*Time.deltaTime);
+            baseTransform.position = Vector3.MoveTowards(baseTransform.position, destination, speed*Time.deltaTime);
+        }
 
-            if (Vector3.Distance(transform.position, destination) < 0.001f) {
-                transform.position = destination;
-                moving = false;
-            }
+        if (Vector3.Distance(baseTransform.position, destination) < 0.01f) {
+            Debug.Log("Update - arrived at destination");
+            baseTransform.position = destination;
+            moving = false;
         }
     }
 
     public void MoveToPoint(Vector3 newDestination) {
+        Debug.Log("MoveToPoint - Start");
         if (!moving) {
+            Debug.Log("MoveToPoint - Inside if not moving");
             destination = new Vector3(
                 newDestination.x,
-                transform.position.y,
+                baseTransform.position.y,
                 newDestination.z
             );
             moving = true;
