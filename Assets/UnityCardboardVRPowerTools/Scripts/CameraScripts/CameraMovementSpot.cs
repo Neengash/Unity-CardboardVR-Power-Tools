2a6bcb78c22306870e2110ee7a550284f4be1771
Neengash <pepmanas93@gmail.com>
2021-06-02 23:23:09 +0200

    wip spotMovement scene demo -- works on eidtor, not the build

---
+++ Assets/UnityCardboardVRPowerTools/Scripts/CameraScripts/CameraMovementSpot.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CameraMovementSpot : MonoBehaviour
+{
+    public static CameraMovementSpot instance;
+    [SerializeField, Range(1f, 10f)] float speed = 1f;
+    private Vector3 destination;
+    private bool moving;
+
+    void Start() {
+        instance = this;
+        moving = false;
+    }
+
+    void Update() {
+        if (moving) {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed*Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, destination) < 0.001f) {
+                transform.position = destination;
+                moving = false;
+            }
+        }
+    }
+
+    public void MoveToPoint(Vector3 newDestination) {
+        if (!moving) {
+            destination = new Vector3(
+                newDestination.x,
+                transform.position.y,
+                newDestination.z
+            );
+            moving = true;
+        }
+    }
+
+}
