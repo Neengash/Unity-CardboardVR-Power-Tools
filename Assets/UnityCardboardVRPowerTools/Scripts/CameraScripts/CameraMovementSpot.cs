b701cc4c8120b9c6c0bc1be92b154e4427716ec9
Neengash <pepmanas93@gmail.com>
2021-06-03 11:10:42 +0200

    Removed logs used for debuging

--- Assets/UnityCardboardVRPowerTools/Scripts/CameraScripts/CameraMovementSpot.cs
+++ Assets/UnityCardboardVRPowerTools/Scripts/CameraScripts/CameraMovementSpot.cs
@@ -13,26 +13,21 @@ public class CameraMovementSpot : MonoBehaviour
     void Start() {
         instance = this;
         moving = false;
-        Debug.Log("Start - speedValue = " + speed);
     }
 
     void Update() {
-        Debug.Log("Update - Start - movingValue = " + moving.ToString());
         if (moving) {
             baseTransform.position = Vector3.MoveTowards(baseTransform.position, destination, speed*Time.deltaTime);
         }
 
         if (Vector3.Distance(baseTransform.position, destination) < 0.01f) {
-            Debug.Log("Update - arrived at destination");
             baseTransform.position = destination;
             moving = false;
         }
     }
 
     public void MoveToPoint(Vector3 newDestination) {
-        Debug.Log("MoveToPoint - Start");
         if (!moving) {
-            Debug.Log("MoveToPoint - Inside if not moving");
             destination = new Vector3(
                 newDestination.x,
                 baseTransform.position.y,
@@ -41,5 +36,4 @@ public class CameraMovementSpot : MonoBehaviour
             moving = true;
         }
     }
-
 }
